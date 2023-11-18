ad9aefd1e0566388472dea7e1b224ad43f3163cc
Marc Delacruz <55219683+itsmarcdelacruz@users.noreply.github.com>
2023-11-17 23:38:12 -0500

    Update settings.py
    Setting up postgresql database

--- financesite/financesite/settings.py
+++ financesite/financesite/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'financesite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'budgetappdb',
+        'USER':'POSTGRES',
+        'PASSWORD': 'pass',
+        'HOST':'localhost',
     }
 }
 
